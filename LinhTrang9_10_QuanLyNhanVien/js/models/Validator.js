c9f9dfa39bed5d3200622a0b54a06885f8cd7b59
Linh Trang <83943885+chanchang24@users.noreply.github.com>
2021-07-05 14:40:36 +0700

    Update Validator.js

--- LinhTrang9_10_QuanLyNhanVien/js/models/Validator.js
+++ LinhTrang9_10_QuanLyNhanVien/js/models/Validator.js
@@ -10,77 +10,78 @@ function Validator() {
     }
 }
 Validator.prototype.checkUsername = function (inputValue, spanID, mess) {
-        var regex = /^\w{4,6}$/;
-        if (!regex.test(inputValue)) {
-            getEle(spanID).style.display = "block";
-            getEle(spanID).innerHTML = mess;
-            return false;
-        }
+    var regex = /^\w{4,6}$/;
+    if (!regex.test(inputValue)) {
+        getEle(spanID).style.display = "block";
+        getEle(spanID).innerHTML = mess;
+        return false;
+    }
+    getEle(spanID).style.display = "none";
+    return true;
+
+}
+Validator.prototype.checkName = function (inputValue, spanID, mess) {
+    var regex = /^[a-zA-Z].*$/;
+    if (!regex.test(inputValue)) {
+        getEle(spanID).style.display = "block";
+        getEle(spanID).innerHTML = mess;
+        return false;
+    }
+    return true;
+}
+Validator.prototype.checkEmail = function (inputValue, spanID, mess) {
+    var regex = /^(\w(\.?\w)*)+@(\w(\.?\w)*)+\.[a-z]{2,10}$/;
+    if (!regex.test(inputValue)) {
+        getEle(spanID).style.display = "block";
+        getEle(spanID).innerHTML = mess;
+        return false;
+    }
+    return true;
+}
+Validator.prototype.checkPassword = function (inputValue, spanID, mess) {
+    var regex = /^(?=.*\d)(?=.*[A-Z])(?=.*[~!@#$%^&*\.]).{6,10}$/;
+    if (!regex.test(inputValue)) {
+        getEle(spanID).style.display = "block";
+        getEle(spanID).innerHTML = mess;
+        return false;
+    }
+    return true;
+}
+Validator.prototype.checkDate = function (inputValue, spanID, mess) {
+    var regex = /^(0?[1-9]|1[012])[/](0?[1-9]|[12][0-9]|3[01])[/](19|20)?[0-9]{2}$/;
+    if (!regex.test(inputValue)) {
+        getEle(spanID).style.display = "block";
+        getEle(spanID).innerHTML = mess;
+        return false;
+    }
+    return true;
+}
+
+Validator.prototype.checkSalary = function (inputValue, spanID, mess) {
+    if (!(inputValue >= 1000000 && inputValue <= 20000000)) {
+        getEle(spanID).style.display = "block";
+        getEle(spanID).innerHTML = mess;
+        return false;
+    }
+    return true;
+}
+Validator.prototype.checkPosition = function (inputValue, spanID, mess) {
+    if (inputValue === "Sếp" || inputValue === "Trưởng phòng" || inputValue === "Nhân viên") {
         getEle(spanID).style.display = "none";
         return true;
-
     }
-    Validator.prototype.checkName = function (inputValue, spanID, mess) {
-        var regex = /^[a-zA-Z].*$/;
-        if (!regex.test(inputValue)) {
-            getEle(spanID).style.display = "block";
-            getEle(spanID).innerHTML = mess;
-            return false;
-        }
+    getEle(spanID).style.display = "block";
+    getEle(spanID).innerHTML = mess;
+    return false;
+}
+Validator.prototype.checkTime = function (inputValue, spanID, mess) {
+    if (inputValue >= 80 && inputValue <= 200) {
+        getEle(spanID).style.display = "none";
         return true;
     }
-    Validator.prototype.checkEmail = function (inputValue, spanID, mess) {
-        var regex = /^(\w(\.?\w)*)+@(\w(\.?\w)*)+\.[a-z]{2,10}$/;
-        if (!regex.test(inputValue)) {
-            getEle(spanID).style.display = "block";
-            getEle(spanID).innerHTML = mess;
-            return false;
-        }
-        return true;
-    }
-    Validator.prototype.checkPassword = function (inputValue, spanID, mess) {
-        var regex = /^(?=.*\d)(?=.*[A-Z])(?=.*[~!@#$%^&*\.]).{6,10}$/;
-        if (!regex.test(inputValue)) {
-            getEle(spanID).style.display = "block";
-            getEle(spanID).innerHTML = mess;
-            return false;
-        }
-        return true;
-    }
-    // this.checkDate = function (inputValue, spanID, mess) {
-    //     var date = new Date(08/14/2021)
-    //     if(date !== inputValue){
-    //         getEle(spanID).style.display = "block";
-    //         getEle(spanID).innerHTML = mess;
-    //         return false;
-    //     }
-    //     return true;
-    // }
-    Validator.prototype.checkSalary = function (inputValue, spanID, mess) {
-        if (!(inputValue >= 1000000 && inputValue <= 20000000)) {
-            getEle(spanID).style.display = "block";
-            getEle(spanID).innerHTML = mess;
-            return false;
-        }
-        return true;
-    }
-    Validator.prototype.checkPosition = function (inputValue, spanID, mess) {
-        if (inputValue === "Sếp" || inputValue === "Trưởng phòng" || inputValue === "Nhân viên") {
-            getEle(spanID).style.display = "none";
-            return true;
-        }
-        getEle(spanID).style.display = "block";
-        getEle(spanID).innerHTML = mess;
-        return false;
-    }
-    Validator.prototype.checkTime = function (inputValue, spanID, mess) {
-        if (inputValue >= 80 && inputValue <= 200) {
-            getEle(spanID).style.display = "none";
-            return true;
-        }
-        getEle(spanID).style.display = "block";
-        getEle(spanID).innerHTML = mess;
-        return false;
-    }
+    getEle(spanID).style.display = "block";
+    getEle(spanID).innerHTML = mess;
+    return false;
+}
 
 
